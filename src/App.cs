71ac971117951932700ab7858d61d592887619d5
Albert <zan15051@gmail.com>
2023-04-21 04:11:22 +0700

    rewrote App.cs so file manipulations work on windows

--- src/App.cs
+++ src/App.cs
@@ -8,23 +8,21 @@ namespace ConsoleTextFileInteraction
 {
     public class App
     {
-        private const int OFFSET = 48;
-        private const string FILE_FOLDER = "txt";
+        private const int KEY_OFFSET = 48;
+        private const string FILES_FOLDER = "notes";
         private const string FILE_FORMAT = ".txt";
-        private static List<FileStream> _files = new();
+        private static List<string> _fileNames;
         private static Menu menu = new Menu(
                 new MenuItem("Создать файл", () => CreateFile()),
                 new MenuItem("Открыть файл", () => ReadFile()),
                 new MenuItem("Удалить файл", () => DeleteFile())
             );
+
         static App()
         {
-            var filenames = new List<string>(Directory.GetFiles(FILE_FOLDER));
-            List<FileStream> files = new();
-            foreach (var fName in filenames)
-                files.Add(new FileStream(fName, FileMode.Open));
-            _files.AddRange(files);
+            _fileNames = new(Directory.GetFiles(FILES_FOLDER));
         }
+        
         public static void Run()
         {
             ConsoleKey pressedKey;
@@ -44,8 +42,9 @@ namespace ConsoleTextFileInteraction
             {
                 menu.ElementAt(GetIndexFromKey(inputToHandle)).InvokeMethod();
             }
-            catch (Exception ex) { }
+            catch (Exception) { }
         }
+
         private static void CreateFile()
         {
             FileStream file;
@@ -55,15 +54,15 @@ namespace ConsoleTextFileInteraction
             string pathAndName = GetFullFilePathWithExtension(fileName);
             if (File.Exists(pathAndName))
             {
-                WriteLineAndHold($"Файл {pathAndName} уже существует!", 2);
+                WriteLineAndHold($"Файл {pathAndName} уже существует!");
                 return;
             }
             if (!TryCreateFile(pathAndName, out file, out errorMessage))
             {
-                WriteLineAndHold("Не удалось добавить файл: " + file?.Name + "\nОшибка:\n" + errorMessage, 4);
+                WriteLineAndHold("Не удалось добавить файл: " + file?.Name + "\nОшибка:\n" + errorMessage);
                 return;
             }
-            WriteLineAndHold("Успешно был создан файл: " + file.Name, 1.5);
+            WriteLineAndHold("Успешно был создан файл: " + file.Name);
         }
         private static bool TryCreateFile(string pathAndName, out FileStream file, out string? errorMessage)
         {
@@ -71,6 +70,7 @@ namespace ConsoleTextFileInteraction
             try
             {
                 file = File.Create(pathAndName);
+                file.Close();
             }
             catch (Exception ex)
             {
@@ -78,85 +78,151 @@ namespace ConsoleTextFileInteraction
                 errorMessage = ex.Message;
                 return false;
             }
-            _files.Add(file);
+            _fileNames = new(Directory.GetFiles(FILES_FOLDER));
+
             return true;
         }
 
         private static void ReadFile()
         {
+            string? errorMessage, content;
             Console.WriteLine("\nВыберите файл для чтения:");
-            var readFile = PrintFiles(0);
-            if (readFile == null) return;
-            using (var reader = new StreamReader(readFile.Name))
+            var fileToRead = PrintFiles();
+            if (fileToRead == null) return;
+            if (!TryReadFile(fileToRead, out content, out errorMessage))
             {
-                var fileContent = reader.ReadToEnd();
-                Console.WriteLine(fileContent);
-                Console.ReadKey();
+                WriteLineAndHold("Не удалось прочитать файл: " + fileToRead + "\nОшибка:\n" + errorMessage);
+                return;
+            }
+            Console.WriteLine("\nСодержимое файла " + fileToRead + ":\n\n" + content);
+
+            // костыль убрать(не уберу)
+            if (Console.ReadKey().Key == ConsoleKey.Backspace)
+            {
+                HandleInput(ConsoleKey.D2);
+            }
+
+        }
+        private static bool TryReadFile(string fileToRead, out string? content, out string? errorMessage)
+        {
+            errorMessage = null;
+            try
+            {
+                content = GetContent();
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                content = String.Empty;
+                return false;
+            }
+            return true;
+            string GetContent()
+            {
+                string fileContent = String.Empty;
+                using (var fs = new FileStream(fileToRead, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var sr = new StreamReader(fs))
+                {
+                    fileContent = sr.ReadToEnd();
+                }
+                return fileContent;
             }
         }
 
         private static void DeleteFile()
         {
+            string? errorMessage;
             Console.WriteLine("\nВыберите файл для для удаления:");
-            var readFile = PrintFiles(0);
-            if (readFile == null) return;
-            File.Delete(readFile.Name);
-            _files.Remove(_files.Where(x => x.Name == readFile.Name).First());
-        }
-
-        private static string GetFullFilePathWithExtension(string fileName) =>
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-         FILE_FOLDER + "\\" + fileName + FILE_FORMAT :
-         FILE_FOLDER + "/" + fileName + FILE_FORMAT;
-        private static string GetFullFilePathWithoutExtension(string fileName) =>
-       RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-        FILE_FOLDER + "\\" + fileName :
-        FILE_FOLDER + "/" + fileName;
-
-        private static void WriteLineAndHold(string message, double secondsToHold)
-        {
-            Console.WriteLine(message);
-            Thread.Sleep(Convert.ToInt32(secondsToHold * 1000));
-        }
-        private static FileStream PrintFiles(int selectedFileIndex)
-        {
-            Console.Clear();
-            menu.Show();
-            int index = 0;
-            foreach (var file in _files)
+            string? fileToDelete = PrintFiles();
+            if (fileToDelete == null) return;
+            if (!TryDeleteFile(fileToDelete, out errorMessage))
             {
-                index++;
-                if (_files.ElementAt(selectedFileIndex) == file)
-                {
-                    Console.WriteLine(">> " + index.ToString() + ". " + file.Name + " <<");
-                    continue;
-                }
-                Console.WriteLine(index.ToString() + ". " + file.Name);
+                WriteLineAndHold("Не удалось удалить файл: " + fileToDelete + "\nОшибка:\n" + errorMessage);
+                return;
             }
-            var chosenFile = HandleChoose();
+            System.Console.WriteLine("Успешно был удалён файл: " + fileToDelete);
+
+            // тот же самый костыль убери(не уберу)
+            if (Console.ReadKey().Key == ConsoleKey.Backspace)
+            {
+                HandleInput(ConsoleKey.D3);
+            }
+        }
+        private static bool TryDeleteFile(string fileToDelete, out string? errorMessage)
+        {
+            errorMessage = null;
+            try
+            {
+                File.Delete(fileToDelete);
+                _fileNames = new(Directory.GetFiles(FILES_FOLDER));
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+                return false;
+            }
+            return true;
+        }
+
+        private static string? PrintFiles(int selectedFileIndex = 0)
+        {
+            ShowMenuAndFiles(selectedFileIndex);
+            string? chosenFile;
+            do
+            {
+                chosenFile = HandleChoose();
+            } while (chosenFile == String.Empty);
             return chosenFile;
 
-            FileStream HandleChoose()
+            void ShowMenuAndFiles(int selectedIndex)
             {
-                ConsoleKey pressedKey = Console.ReadKey(false).Key;
-                if (pressedKey != ConsoleKey.Enter) HandleInput();
-                return _files.ElementAt(selectedFileIndex);
-
-
-                void HandleInput()
+                Console.Clear();
+                menu.Show();
+                Console.WriteLine();
+                int numeration = 0;
+                foreach (string file in _fileNames)
                 {
-                    switch (pressedKey)
+                    numeration++;
+                    if (_fileNames.ElementAt(selectedIndex) == file)
                     {
-                        case ConsoleKey.UpArrow: PrintFiles(IsValidIndex(--selectedFileIndex) ? selectedFileIndex : ++selectedFileIndex); break;
-                        case ConsoleKey.DownArrow: PrintFiles(IsValidIndex(++selectedFileIndex) ? selectedFileIndex : --selectedFileIndex); break;
-                        case ConsoleKey.Escape: break;
-                        default: HandleInput(); break;
+                        Console.WriteLine(">> " + numeration.ToString() + ". " + file + " <<");
+                        continue;
                     }
+                    Console.WriteLine(numeration.ToString() + ". " + file);
                 }
             }
-            bool IsValidIndex(int indx) => (indx >= 0 && indx <= _files.Count - 1);
+            string? HandleChoose()
+            {
+                ConsoleKey pressedKey = Console.ReadKey(false).Key;
+                if (pressedKey == ConsoleKey.Escape || pressedKey == ConsoleKey.Backspace) return null;
+                if (pressedKey == ConsoleKey.Enter) return _fileNames.ElementAt(selectedFileIndex);
+                HandleInput(pressedKey);
+                return String.Empty;
+            }
+            void HandleInput(ConsoleKey input)
+            {
+                switch (input)
+                {
+                    case ConsoleKey.UpArrow: ShowMenuAndFiles(IsValidIndex(--selectedFileIndex) ? selectedFileIndex : ++selectedFileIndex); break;
+                    case ConsoleKey.DownArrow: ShowMenuAndFiles(IsValidIndex(++selectedFileIndex) ? selectedFileIndex : --selectedFileIndex); break;
+                    default: ShowMenuAndFiles(selectedFileIndex); break;
+                }
+            }
         }
-
-        private static int GetIndexFromKey(ConsoleKey key) => (int)key - OFFSET;
+       
+        private static bool IsValidIndex(int indx) => (indx >= 0 && indx <= _fileNames.Count - 1);
+        private static int GetIndexFromKey(ConsoleKey key) => (int)key - KEY_OFFSET;
+        private static string GetFullFilePathWithExtension(string fileName) => GetFullFilePathWithoutExtension(fileName) + FILE_FORMAT;
+        private static string GetFullFilePathWithoutExtension(string fileName) => RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? FILES_FOLDER + "\\" + fileName : FILES_FOLDER + "/" + fileName;
+        private static void WriteLineAndHold(string message, double secondsToHold = Double.NaN)
+        {
+            Console.WriteLine(message);
+            if (Double.IsNaN(secondsToHold))
+            {
+                Console.ReadKey();
+                return;
+            }
+            Thread.Sleep(Convert.ToInt32(secondsToHold * 1000));
+        }
     }
 }
