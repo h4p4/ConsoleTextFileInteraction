c5ded3ff2847a4a53589db4e063f3826f01f35f4
Albert <zan15051@gmail.com>
2023-04-21 23:12:37 +0700

    cyrillic now available in file names on russian windows

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace ConsoleTextFileInteraction{
-    public class Program {
-        static void Main(String [] args){
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleTextFileInteraction{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            SetEncoding();
             App.Run();
+        }
+        static void SetEncoding(){
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                Console.OutputEncoding = Encoding.GetEncoding(866);
+                Console.InputEncoding = Encoding.GetEncoding(866);
+            }
         }
     }
 }
